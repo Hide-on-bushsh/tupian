355bb909d061dc09db3c586bbba08984aef1b423
Hide-on-bushsh <978716854@qq.com>
2025-12-01 15:07:10 +0800

    k

--- req.py
+++ req.py
@@ -1,23 +1,23 @@
 import requests
 
-url = "http://127.0.0.1:30000"
+url = "http://127.0.0.1:8080"
 
 payload = {
     "model": "/data1/models/bge-m3",
-    "input": "What is the capital of France?",
+    "input": "How are you",
     "encoding_format": "float"
 }
 
 response = requests.post(url + "/v1/embeddings", json=payload).json()
 embedding = response["data"][0]["embedding"]
 
-payload = {
+payload1 = {
     "model": "/data1/models/bge-m3",
-    "input": "What is the capital of BeiJing?",
+    "input": "I am fine",
     "encoding_format": "float"
 }
 
-response1 = requests.post(url + "/v1/embeddings", json=payload).json()
+response1 = requests.post(url + "/v1/embeddings", json=payload1).json()
 embedding1 = response1["data"][0]["embedding"]
 for a,b in zip(embedding, embedding1):
     print(f"{a-b}\t{a}\t{b}")
--- run_bge.sh
+++ run_bge.sh
@@ -1,1 +1,2 @@
-ASCEND_RT_VISIBLE_DEVICES=4 python3 -m sglang.launch_server   --model-path /data1/models/bge-m3   --host 0.0.0.0   --disable-radix-cache   --chunked-prefill-size -1   --attention-backend ascend   --is-embedding   --port 30000
+export PYTHONPATH=/data/x30060150/sgl-project/sglang/python:$PYTHONPATH
+ASCEND_RT_VISIBLE_DEVICES=4 python3 -m sglang.launch_server   --model-path /data/ascend-ci-share-pkking-sglang/modelscope/hub/models/BAAI/bge-m3   --host 127.0.0.1   --disable-radix-cache   --chunked-prefill-size -1   --attention-backend ascend   --is-embedding   --port 30000
